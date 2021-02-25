e67e473c0e347080da029d5866f6537aa1c88a24
samiwokx <samsaravillo@gmail.com>
2021-02-26 00:27:50 +0800

    update author and courses component

--- src/app/dashboard/courses/courses.component.ts
+++ src/app/dashboard/courses/courses.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { CoursesService } from "./courses.service";
 
 @Component({
-  selector: "app-courses",
+  selector: "courses",
   templateUrl: "./courses.component.html",
   styleUrls: ["./courses.component.scss"],
 })
